9915dd0ebb6403e81934488ff7f3cf32c13cb8ef
Emerson de Abreu <mersoabreu@gmail.com>
2022-01-08 21:02:51 -0300

    Criando o projeto

---
+++ hhmoney-api/src/main/java/com/hhsoftware/moneyapi/HhmoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.hhsoftware.moneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhmoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhmoneyApiApplication.class, args);
+	}
+
+}
---
+++ hhmoney-api/src/test/java/com/hhsoftware/moneyapi/HhmoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhsoftware.moneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhmoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
